fd6e06c5d47113c1f819b8590d3c977fd3e57a7b
Simonebaca <simobaco99@gmail.com>
2023-03-14 12:14:57 +0100

    snack2

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ L’utente inserisce due numeri in successione, con due prompt.
 Il software stampa il maggiore.*/
 
 
-const firstNumber = Number(prompt("Insert the first number"));
+/*const firstNumber = Number(prompt("Insert the first number"));
 const secondNumber = Number(prompt("Insert the second number"));
 
 if (isNaN(firstNumber) || isNaN(secondNumber)) {
@@ -16,4 +16,23 @@ if (isNaN(firstNumber) || isNaN(secondNumber)) {
     } else {
       console.log(`The second number ${secondNumber} is bigger than the first ${firstNumber}`);
     }
+}*/
+
+
+/*L’utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga.*/
+
+const firstWord =  prompt("Insert the first word");
+const secondWord = prompt("Insert the second word");
+
+if (firstWord.length == 0 || secondWord.length == 0) {
+  alert("You should insert two words of, at least, one character each");
+} else {
+  if (firstWord.length == secondWord.length) {
+    console.log("The two words have the same length");
+  } else if (firstWord.length > secondWord.length) {
+    console.log(`The first word "${firstWord}" is longer than the second "${secondWord}" so the right order is: ${secondWord}, ${firstWord}`)
+  } else {
+    console.log(`The second word "${secondWord}" is longer than the first "${firstWord}" so the right order is: ${firstWord}, ${secondWord}`)
+  }
 }
