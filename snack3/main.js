12c2a391fd8205591bb0e1bc290bdb6ada3fb144
Simonebaca <simobaco99@gmail.com>
2023-03-15 12:11:45 +0100

    while-snack3

--- snack3/main.js
+++ snack3/main.js
@@ -2,11 +2,21 @@
 Il programma stampa la somma di tutti i numeri inseriti.*/
 
 
-let sum = 0;
+/*let sum = 0;
 
 for (let i = 1; i <= 10; i++) {
   const number = Number(prompt(`${i}) Insert a number`));
   isNaN(number) ? i-- : sum += number;
 }
-console.log(`The sum of the inserted numbers is ${sum}`);
+console.log(`The sum of the inserted numbers is ${sum}`);*/
 
+
+let sumWhile = 0;
+let j = 0;
+
+while (j < 10) {
+  let number = Number(prompt(`${j}) Insert a number`));
+    sumWhile += number;
+  j++;
+}
+console.log(sumWhile);
